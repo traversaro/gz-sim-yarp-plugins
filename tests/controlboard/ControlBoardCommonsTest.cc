86e4d2093706532f9e88d8cc2e9de9b0d1fd2d6e
Silvio <silvio.traversaro@iit.it>
2025-02-09 10:47:29 +0100

    Add tests for configurationoverride plugin

--- tests/controlboard/ControlBoardCommonsTest.cc
+++ tests/controlboard/ControlBoardCommonsTest.cc
@@ -1,6 +1,7 @@
 #include "ControlBoardDriver.hh"
 #include <ControlBoardData.hh>
 #include <DeviceRegistry.hh>
+#include <Common.hh>
 
 #include <gtest/gtest.h>
 
@@ -33,29 +34,66 @@ namespace gzyarp
 namespace test
 {
 
-// Checks that the control board can be configured without initial conditions
-TEST(ControlBoardCommonsTest, ConfigureControlBoardWithoutInitialCondition)
-{
-    auto modelPath
-        = std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) / "pendulum_no_initial_configuration.sdf";
+// Define a struct to hold the test parameters for initial configuration tests
+struct ControlBoardInitialConfigurationTestsParams {
+    std::string modelFileName;
+    std::vector<double> expectedJointInitialPosInRad;
+};
+
+// Define the test fixture with parameters
+class ControlBoardInitialConfigurationTest : public ::testing::TestWithParam<ControlBoardInitialConfigurationTestsParams> {
+};
+
+// Define the parameterized test
+TEST_P(ControlBoardInitialConfigurationTest, CheckInitialConfiguration) {
+    // Get the parameters for this test instance
+    const ControlBoardInitialConfigurationTestsParams& params = GetParam();
+
+    // Construct the model path
+    auto modelPath = std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) / params.modelFileName;
     gz::sim::TestFixture testFixture(modelPath.string());
 
     gz::common::Console::SetVerbosity(4);
 
-    testFixture.Finalize();
-}
-
-// Checks that the control board can be configured without initial conditions
-TEST(ControlBoardCommonsTest, ConfigureControlBoardWithInitialCondition)
-{
-    auto modelPath = std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR)
-                     / "pendulum_with_initial_configuration.sdf";
-    gz::sim::TestFixture testFixture(modelPath.string());
+    yarp::dev::IEncoders* iencs = nullptr;
 
     gz::common::Console::SetVerbosity(4);
 
+    testFixture.OnConfigure([&](const gz::sim::Entity& _worldEntity,
+                                const std::shared_ptr<const sdf::Element>& /*_sdf*/,
+                                gz::sim::EntityComponentManager& _ecm,
+                                gz::sim::EventManager& /*_eventMgr*/) {
+        auto deviceIds = DeviceRegistry::getHandler()->getDevicesKeys(_ecm);
+        ASSERT_TRUE(deviceIds.size() == 1);
+
+        yarp::dev::PolyDriver* driver = nullptr;
+        EXPECT_TRUE(DeviceRegistry::getHandler()->getDevice(_ecm, deviceIds[0], driver));
+        ASSERT_TRUE(driver != nullptr);
+        ASSERT_TRUE(driver->view(iencs));
+    });
+
     testFixture.Finalize();
+
+    for (int i = 0; i < params.expectedJointInitialPosInRad.size(); i++) {
+        double initialPosInDeg;
+        iencs->getEncoder(i, &initialPosInDeg);
+        EXPECT_DOUBLE_EQ(initialPosInDeg, gzyarp::convertRadiansToDegrees(params.expectedJointInitialPosInRad[i]));
+    }
 }
+
+// Instantiate the test suite with different parameters
+INSTANTIATE_TEST_SUITE_P(
+    ControlBoardTests,
+    ControlBoardInitialConfigurationTest,
+    ::testing::Values(
+        // Temporary disabled
+        // ControlBoardInitialConfigurationTestsParams{"pendulum_with_doubly_overriden_initial_configuration.sdf", {2.0}}
+        ControlBoardInitialConfigurationTestsParams{"pendulum_with_overriden_initial_configuration.sdf", {2.0}},
+        ControlBoardInitialConfigurationTestsParams{"pendulum_no_initial_configuration.sdf", {0.0}},
+        ControlBoardInitialConfigurationTestsParams{"pendulum_with_initial_configuration.sdf", {1.0}}
+    )
+);
+
 
 // Check that multiple control board can be congfigured for the same robot model
 TEST(ControlBoardCommonsTest, ConfigureMultipleControlBoards)
